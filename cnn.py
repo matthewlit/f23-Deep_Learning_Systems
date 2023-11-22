a0aad3a00e242a11aec4970ba4e5b86f2f822bcc
Matthew Kelleher <Matthew.kelleher10@comcast.net>
2023-11-22 02:23:20 -0500

    Implemented a simple CNN with 49% accuracy

--- cnn.py
+++ cnn.py
@@ -1,4 +1,7 @@
 import dataset
+import tensorflow as tf
+from keras import layers, losses
+from sklearn import preprocessing
 
 """
 Convolutional Neural Network for Song Genre Classification Based on Spotify API Data
@@ -6,11 +9,37 @@ Convolutional Neural Network for Song Genre Classification Based on Spotify API 
 
 def cnn():
     #Preprocess dataset for CNN
+    print("Preprocessing...")
     trainx, testx, trainy, testy = preprocess()
 
-    #TODO: Implement CNN
+    #Implement CNN model
+    print("Building Model...")
+    input_shape = (trainx.shape[1],1)
 
-    #TODO: Test CNN
+    # TODO: Find best layer architecture for highest accuracy 
+    # HIGH: 49% 
+    model = tf.keras.Sequential([
+        layers.Conv1D(64, 2, activation='relu', input_shape=input_shape),
+        layers.Conv1D(64, 2, activation='relu'),
+        layers.MaxPooling1D(),
+        layers.Conv1D(128, 2, activation='relu'),
+        layers.Conv1D(128, 2, activation='relu'),
+        layers.MaxPooling1D(),
+        layers.Flatten(),
+        layers.Dense(500, activation='relu'),
+        layers.Dense(6, activation='softmax')
+    ])
+
+    model.compile(optimizer='Adam', loss=losses.CategoricalCrossentropy(), metrics=['accuracy'])
+    model.summary()
+
+    print("\nTraining...")
+    model.fit(trainx, trainy, epochs=8)
+
+    #Test CNN
+    print("\nTesting...")
+    results = model.evaluate(testx, testy)
+    print("Results [Loss, Accuracy]: ", results)
 
 
 #Preprocess dataset for CNN
@@ -18,7 +47,10 @@ def preprocess():
     #Get train and test dataset
     trainx, testx, trainy, testy = dataset.getData()
 
-    #TODO: Preprocess for CNN
+    #Encode categorical features
+    enc = preprocessing.OneHotEncoder()
+    trainy = enc.fit_transform(trainy).toarray()
+    testy = enc.fit_transform(testy).toarray()
 
     return  trainx, testx, trainy, testy
 
--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 
 """
@@ -8,18 +9,18 @@ Imports data from spotify_songs.csv and splits desired data into train and test 
 def getData():
     #Import csv and get desired columns  
     data = pd.read_csv('spotify_songs.csv', sep=',')
-    data = data[["danceability", "energy" ,"key" ,"loudness" ,"mode" ,"speechiness" ,"acousticness" ,"instrumentalness" ,"liveness" ,"valence" ,"tempo" ,"playlist_genre"]]
+    data = data[["danceability", "energy" ,"key", "mode" ,"speechiness" ,"acousticness" ,"instrumentalness" ,"liveness" ,"valence" ,"tempo" , "duration_ms", "playlist_genre"]]
 
     #Split into train and test
     trainx, testx, trainy, testy = train_test_split(
-        data[["danceability", "energy" ,"key" ,"loudness" ,"mode" ,"speechiness" ,"acousticness" ,"instrumentalness" ,"liveness" ,"valence" ,"tempo"]], 
+        data[["danceability", "energy" ,"key", "mode" ,"speechiness" ,"acousticness" ,"instrumentalness" ,"liveness" ,"valence" ,"tempo", "duration_ms"]], 
         data[["playlist_genre"]], 
         test_size=0.2)
-
-    # print(trainx)
-    # print(trainy)
-    # print(testx)
-    # print(testy)
+    
+    trainx = trainx.to_numpy()
+    testx = testx.to_numpy()
+    trainy = trainy.to_numpy()
+    testy = testy.to_numpy()
 
     return trainx, testx, trainy, testy
 

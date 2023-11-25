bfbd226945f4eb45da762dc632d6c1b6402b5352
Matthew Kelleher <Matthew.kelleher10@comcast.net>
2023-11-25 01:39:44 -0500

    Implemented a simple RNN with 53% accuracy

--- rnn.py
+++ rnn.py
@@ -1,4 +1,6 @@
-import dataset
+import preprocess
+import tensorflow as tf
+from keras import layers, losses
 
 """
 Recurrent Neural Network for Song Genre Classification Based on Spotify API Data
@@ -6,22 +8,31 @@ Recurrent Neural Network for Song Genre Classification Based on Spotify API Data
 
 def rnn():
     #Preprocess dataset for RNN
-    trainx, testx, trainy, testy = preprocess()
+    print("\nPreprocessing...")
+    trainx, testx, trainy, testy = preprocess.getData()
 
-    #TODO: Implement RNN
+    #Implement RNN model
+    print("\nBuilding Model...")
+    input_shape = (trainx.shape[1],1)
 
-    #TODO: Test RNN
-    
-    
-#Preprocess dataset for RNN
-def preprocess():
-    #Get train and test datasets
-    trainx, testx, trainy, testy = dataset.getData()
+    # TODO: Find best layer architecture for highest accuracy 
+    # HIGH: 53% 
+    model = tf.keras.Sequential([
+        layers.Bidirectional(layers.GRU(64, activation='relu', return_sequences=True), input_shape=input_shape),
+        layers.Bidirectional(layers.GRU(128)),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(6, activation='softmax')
+    ])
 
-    #TODO: Preprocess for RNN
+    model.compile(optimizer='Adam', loss=losses.CategoricalCrossentropy(), metrics=['accuracy'])
 
-    return  trainx, testx, trainy, testy
+    print("\nTraining...")
+    model.fit(trainx, trainy, epochs=8)
 
+    #Test RNN
+    print("\nTesting...")
+    results = model.evaluate(testx, testy)
+    print("\nResults [Loss, Accuracy]: ", results)
 
 if __name__ == '__main__':
     rnn()
